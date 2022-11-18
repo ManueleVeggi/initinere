03f58c1eba6b08a09397d34c2ef6c4eb1d7d704c
ManueleVeggi <manuele.veggi@studio.unibo.it>
2022-11-18 20:59:30 +0100

    Check len raw csv
    It shows that the even the original csv file have different length (comparison dsu-dsu, fee-fee etc year per year)

--- temp_brainstorming/info.py
+++ temp_brainstorming/info.py
@@ -28,3 +28,63 @@ def sumintern(colname):
     return mySeries
 
 print(sumintern("total_students"), sumintern("total_students").mean())
+
+
+#Following code shows that df are not same legth even at the beginning
+
+""" #code
+def compare(d1, d2, d3, d4, year):
+    df1 = pd.read_csv(d1, sep=";", encoding = "ISO-8859-1")
+    df2 = pd.read_csv(d2, sep=";", encoding = "ISO-8859-1")
+    df3 = pd.read_csv(d3, sep=",", encoding = "ISO-8859-1").query('AnnoA == "{0}"'.format(year))
+    df4 = pd.read_csv(d4, sep=",", encoding = "ISO-8859-1").query('AnnoA == "{0}"'.format(year))
+    print(year)
+    d = {
+        "dsu": len(df1),
+        "fee": len(df2),
+        "stu": len(df3),
+        "int": len(df4)
+    }
+    return d
+
+stud = "data/student.csv"
+ints = "data/intStudent.csv"
+
+dsu2016 = "data/dsu2016.csv"
+fee2016 = "data/fees2016.csv"
+dest2016 = "data/output/2016.csv"
+
+dsu2017 = "data/dsu2017.csv"
+fee2017 = "data/fees2017.csv"
+dest2017 = "data/output/2017.csv"
+
+dsu2018 = "data/dsu2018.csv"
+fee2018 = "data/fees2018.csv"
+dest2018 = "data/output/2018.csv"
+
+dsu2019 = "data/dsu2019.csv"
+fee2019 = "data/fees2019.csv"
+dest2019 = "data/output/2019.csv"
+
+print(compare(dsu2016, fee2016, stud, ints, "2015/2016"))
+print("*****************")
+print(compare(dsu2017, fee2017, stud, ints, "2016/2017"))
+print("*****************")
+print(compare(dsu2018, fee2018, stud, ints, "2017/2018"))
+print("*****************")
+print(compare(dsu2019, fee2019, stud, ints, "2018/2019"))
+"""
+
+""" #results
+2015/2016
+{'dsu': 3470, 'fee': 99, 'stu': 182, 'int': 91}
+*****************
+2016/2017
+{'dsu': 3582, 'fee': 100, 'stu': 182, 'int': 91}
+*****************
+2017/2018
+{'dsu': 4074, 'fee': 100, 'stu': 182, 'int': 91}
+*****************
+2018/2019
+{'dsu': 4072, 'fee': 95, 'stu': 184, 'int': 92}
+"""
